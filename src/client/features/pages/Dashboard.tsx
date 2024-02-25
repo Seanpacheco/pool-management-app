746508de1afe31cca38358f4dd7ec3c7957653a7
Sean Pacheco <seanpacheco.code@gmail.com>
2024-02-25 17:49:45 -0500

    [#7]Add dashboard page and route -add dashboard page to feature folder -add route to dashboard page in routes/index.tsx -add main layout to Dashboard page

---
+++ src/client/features/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { MainLayout } from '../../../client/components/Layout/MainLayout';
+
+export function Dashboard() {
+  return (
+    <MainLayout>
+      <h1>Dashboard</h1>
+    </MainLayout>
+  );
+}
