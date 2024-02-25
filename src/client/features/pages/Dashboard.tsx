3e68d40a906a8fcaffe2c55eaf1964f2ef1aa31e
Sean Pacheco <seanpacheco.code@gmail.com>
2024-02-25 17:52:50 -0500

    [#7]Add Dashboard and Route

--- src/client/features/pages/Dashboard.tsx
+++ src/client/features/pages/Dashboard.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { MainLayout } from '../../../client/components/Layout/MainLayout';
 
-export function Dashboard() {
+export const Dashboard = () => {
   return (
     <MainLayout>
       <h1>Dashboard</h1>
     </MainLayout>
   );
-}
+};
