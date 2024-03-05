52f6d5c255d486b777775a6c89c3346b7e4cb96e
Sean Pacheco <seanpacheco.code@gmail.com>
2024-03-05 16:09:51 -0500

    [#16]add auth to api endpoint - add auth0 middleware - add error middleware - add not found middleware - update main.ts

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,54 @@
-import express from "express";
-import ViteExpress from "vite-express";
+import express from 'express';
+import ViteExpress from 'vite-express';
+import { validateAccessToken } from './middleware/auth0.middleware';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+// app.use(
+//   auth({
+//     issuerBaseURL: domain,
+//     audience: audience,
+//   }),
+// );
+
+app.get('/hello', (_, res) => {
+  res.send('Hello Vite + React + TypeScript!');
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
-);
+app.get('/api/v1/accounts', validateAccessToken, (req, res) => {
+  console.log('getting accounts');
+  res.status(200).json({
+    status: 'success',
+    data: [
+      {
+        id: '1',
+        name: 'Alex Santos',
+        state: 'HI',
+        sites: 3,
+      },
+      {
+        id: '2',
+        name: 'Viva Wittman',
+        state: 'HI',
+        sites: 4,
+      },
+      {
+        id: '3',
+        name: 'Cliff Pacheco',
+        state: 'HI',
+        sites: 1,
+      },
+      {
+        id: '4',
+        name: 'Dywane Wade',
+        state: 'HI',
+        sites: 5,
+      },
+    ],
+  });
+});
+
+ViteExpress.listen(app, 3000, () => console.log('Server is listening on port 3000...'));
